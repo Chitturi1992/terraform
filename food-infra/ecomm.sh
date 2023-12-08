e7e06e80174a1bae587a90c0599f03f5b91c5221
Chitturi1992 <150113985+Chitturi1992@users.noreply.github.com>
2023-12-08 22:23:03 +0530

    Update and rename food.sh to ecomm.sh

--- food-infra/ecomm.sh
+++ food-infra/ecomm.sh
@@ -3,4 +3,4 @@
 sudo apt update -y
 sudo apt install -y nginx
 sudo rm /var/www/html/*.html
-sudo git clone https://github.com/Chitturi1992/food.git /var/www/html
+sudo git clone https://github.com/Chitturi1992/ecomm.git /var/www/html
